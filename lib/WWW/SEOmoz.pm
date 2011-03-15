b91a57089a1051c4219fb5b94619517d0804c24b
Adam Taylor <adamjctaylor@gmail.com>
2011-03-15 23:58:27 +0000

    Fix authentication and start url-metrics method

--- lib/WWW/SEOmoz.pm
+++ lib/WWW/SEOmoz.pm
@@ -4,7 +4,7 @@ use Moose;
 use LWP::UserAgent;
 use DateTime;
 use URI::Escape;
-use Digest::SHA qw( hmac_sha1 );
+use Digest::SHA qw( hmac_sha1_base64 );
 
 has access_id => (
     is       => 'ro',
@@ -41,17 +41,45 @@ sub _build_ua {
 sub _build_api_url {
     my $self = shift;
 
-    return $API_BASE . 'linkscape';
+    return $API_BASE . 'linkscape/';
 }
 
 sub _generate_authentication {
     my $self = shift;
 
-    my $epoch = DateTime->now->add( seconds => 30 )->epoch; # A bit in the future
-    my $sig = uri_escape(hmac_sha1_base64(
+    my $epoch = DateTime->now->add( seconds => 60 )->epoch; # A bit in the future
+    my $sig = hmac_sha1_base64(
         $self->access_id . "\n" . $epoch, $self->secret_key
-    ));
-    return 'AccessID='.$self->access_id.'&Expires='.$epoch.'&Signature='.$sig;
+    );
+
+    # Pad the base_64 encoding, if required
+    while (length($sig) % 4) {
+        $sig .= '=';
+    }
+
+    $sig = uri_escape( $sig );
+
+    warn $sig;
+
+    return '?AccessID='.$self->access_id.'&Expires='.$epoch.'&Signature='.$sig;
+}
+
+sub url_metrics {
+    my $self = shift;
+    my ( $url ) = @_;
+
+    my $api_call = $self->api_url
+        . 'url-metrics/'
+        . uri_escape($url)
+        . $self->_generate_authentication;
+
+    my $res = $self->ua->get( $api_call );
+
+    warn pp $res->content;
+
+    if ( $res->is_success ) {
+        return $res->content;
+    }
 }
 
 1;
---
+++ t/01-load.t
@@ -0,0 +1,4 @@
+use Test::More tests => 2;
+
+BEGIN { use_ok( 'WWW::SEOmoz' ); }
+require_ok( 'WWW::SEOmoz' );
